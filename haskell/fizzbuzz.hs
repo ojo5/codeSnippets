6238d0ed1634dddc60de4a72294239908e672a52
ojo5 <johansen949@gmail.com>
2024-08-31 16:39:55 +0200

    Create fizzbuzz

---
+++ haskell/fizzbuzz.hs
@@ -0,0 +1,9 @@
+fizzBuzz :: Int -> String
+fizzBuzz n
+    | n `mod` 15 == 0 = "FizzBuzz"
+    | n `mod` 3 == 0  = "Fizz"
+    | n `mod` 5 == 0  = "Buzz"
+    | otherwise       = show n
+
+main :: IO ()
+main = mapM_ (putStrLn . fizzBuzz) [1..100]
